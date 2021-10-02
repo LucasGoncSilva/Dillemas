9dd1d495164a7f85c54ece544b032e87275218b5
LucasGoncSilva <lucasgoncsilva04@gmail.com>
2021-10-02 16:43:28 -0300

    almost functional fetching system

--- scripts.js
+++ scripts.js
@@ -1,10 +1,31 @@
-window.addEventListener('DOMContentLoaded', () => {
+function fetching(url) {
 
-    document.querySelector('#confirm').onclick = () => {
-
-        fetch('/Inicial/INSTRUCOES.txt')
+    fetch(url)
         .then(response => response.text())
         .then(response => document.querySelector('#content').innerHTML = response)
+
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#answer').onsubmit = () => {
+
+        if (document.querySelector('#submit')) {
+
+            document.querySelectorAll('input[name="choice"]').forEach((elem) => {
+
+                if (elem.checked) { fetching(elem.value) }
+
+            })
+
+        } else {
+
+            fetching('/Inicial/cena0.txt')
+
+        }
+
+        return false
 
     }
 
