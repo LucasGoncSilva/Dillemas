f17e3da8b40007b875d584da055a0475e4fb8854
LucasGoncSilva <lucasgoncsilva04@gmail.com>
2021-10-04 20:40:48 -0300

    final idea

--- scripts.js
+++ scripts.js
@@ -1,19 +1,10 @@
-function fetching(url) {
-
-    fetch(url)
-        .then(response => response.text())
-        .then(response => document.querySelector('#content').innerHTML = response)
-
-}
-
-
 window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#answer').onsubmit = () => {
 
         document.querySelectorAll('input[name="choice"]').forEach((elem) => {
 
-            if (elem.checked) { fetching(elem.value) }
+            if (elem.checked) { window.location.replace(elem.value) }
 
         })
 
