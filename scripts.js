9b669f281b43356de00fc94d97908218a63e2fa1
LucasGoncSilva <lucasgoncsilva04@gmail.com>
2021-10-04 23:33:36 -0300

    half formatted scenes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
+const wrong_scenes = [
+    '../Intermediario/cena1d1a.html',
+    '../Intermediario/cena1d1c.html',
+]
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#answer').onsubmit = () => {
 
         document.querySelectorAll('input[name="choice"]').forEach((elem) => {
 
-            if (elem.checked) { window.location.replace(elem.value) }
+            if (elem.checked) {
+
+                window.location.href = elem.value
+
+            }
 
         })
 
