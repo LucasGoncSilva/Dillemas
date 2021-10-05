a129deddd0baecc69ecf4c86d994f508370e2435
LucasGoncSilva <lucasgoncsilva04@gmail.com>
2021-10-05 00:47:54 -0300

    url fixing

--- scripts.js
+++ scripts.js
@@ -2,35 +2,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const url = (window.location.host)
 
-    console.log(url)
-
-    if ('github' in url) {
-        console.log('its in')
-    }
-
     document.querySelector('#answer').onsubmit = () => {
 
         document.querySelectorAll('input[name="choice"]').forEach((elem) => {
 
             if (elem.checked) {
 
-                // if ('github' in url) {
-                    
-                // } else {
-                    
-                // }
-
-                window.location.replace(`${elem.value}`)
-
-                // switch ('github' in url) {
-                //     case true:
-                //         window.location.replace(`${url}/Dillemas/${elem.value}`)
-                //         break;
+                switch (url.includes('github')) {
+                    case true:
+                        window.location.pathname(`/Dillemas/${elem.value}`)
+                        break;
                 
-                //     case false:
-                //         window.location.replace(`${elem.value}`)
-                //         break;
-                // }
+                    case false:
+                        window.location.replace(`${elem.value}`)
+                        break;
+                }
 
             }
 
