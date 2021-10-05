17631853da4dae0545f9cb792fd8f4965d1ec74e
LucasGoncSilva <lucasgoncsilva04@gmail.com>
2021-10-05 01:01:28 -0300

    url fixed

--- scripts.js
+++ scripts.js
@@ -2,13 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const url = (window.location.host)
 
-    if (url.includes('github')) {
-        console.log(window.location.pathname)
-    } else {
-        console.log('nope')
-        console.log(window.location.pathname)
-    }
-
     document.querySelector('#answer').onsubmit = () => {
 
         document.querySelectorAll('input[name="choice"]').forEach((elem) => {
@@ -17,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 switch (url.includes('github')) {
                     case true:
-                        window.location.pathname = `/Dillemas/${elem.value}`
+                        window.location.pathname = `/Dillemas${elem.value}`
                         break;
 
                     case false:
